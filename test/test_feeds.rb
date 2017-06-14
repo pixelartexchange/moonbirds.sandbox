d42af0bc5d32e4e4efc89619a05d608258d0e4e2
Gerald Bauer <gerald.bauer@gmail.com>
2017-06-14 22:05:57 +0200

    add guardian

--- test/test_feeds.rb
+++ test/test_feeds.rb
@@ -12,15 +12,38 @@ require  'helper'
 class TestFeeds < MiniTest::Test
 
   def test_all
+     ## walk_feeds( '.' )
 
-    walk( '.' ) do |path|
+     parse_feeds( './news/guardian*' )
+  end  # method test_all
+
+
+
+private
+
+def walk_feeds( root='.' )
+    walk( root ) do |path|
 
        ## note: skip README, Rakefile etc.
        ##    check for extensions
        extname = File.extname( path )    # note: includes dot e.g. .json etc.
        next unless ['.json', '.html', '.xml', '.rss', '.rss2', '.atom'].include?( extname )
 
-  
+       parse_feed( path )
+    end
+end # walk_feeds
+
+
+def parse_feeds( pattern )
+  files = Dir.glob( pattern )
+  files.each do |path|
+      puts "  #{path}"
+      parse_feed( path )
+  end
+end
+
+
+def parse_feed( path )
        puts "  reading #{path} ..."
 
        b = BlockReader.from_file( path ).read
@@ -35,7 +58,6 @@ class TestFeeds < MiniTest::Test
        else
          assert_feed( text, tests )
        end
-    end
-  end  # method test_all
+end
 
 end # class TestFeeds
