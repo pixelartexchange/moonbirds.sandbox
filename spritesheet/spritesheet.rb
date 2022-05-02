8288baae55ea634122d0eb53524c3c21019252e8
Gerald Bauer <gerald.bauer@gmail.com>
2022-05-02 16:56:07 +0200

    up

--- spritesheet/spritesheet.rb
+++ spritesheet/spritesheet.rb
@@ -1,68 +1,35 @@
-#####
-# to run use:
-#   $ ruby proof-moonbirds/spritesheet.rb
+####################
+#  assemble spritesheet (all-in-one composite image with attributes)
+#
+#  to run use:
+#    $ ruby spritesheet/spritesheet.rb
+
 
 require 'pixelart'
+require 'csvreader'
 
 
 
+attributes = CsvHash.read( "./spritesheet/attributes.csv" )
+puts "  #{attributes.size} record(s)"  #=> 82 record(s)
 
-spritesheet = ImageComposite.new( 10, 5, width: 42, height: 42 )
 
-['eyewear-3d_glasses',
- 'eyewear-black-rimmed_glasses',
- 'eyewear-eyepatch',
- 'eyewear-gazelles',
- 'eyewear-jobs_glasses',
- 'eyewear-rose-colored_glasses',
- 'eyewear-sunglasses',
- 'eyewear-visor',
- 'headwear-aviators_cap',
- 'headwear-backwards_hat',
- 'headwear-bandana',
- 'headwear-beanie',
- 'headwear-bow',
- 'headwear-bucket_hat',
- 'headwear-captains_cap',
- 'headwear-chromie',
- 'headwear-cowboy_hat',
- 'headwear-crescent_talisman',
- 'headwear-dancing_flame',
- 'headwear-diamond',
- 'headwear-durag',
- 'headwear-fire',
- 'headwear-flowers',
- 'headwear-forest_ranger',
- 'headwear-grail',
- 'headwear-gremplin',
- 'headwear-halo',
- 'headwear-headband',
- 'headwear-headphones',
- 'headwear-heros_cap',
- 'headwear-karate_band',
- 'headwear-lincoln',
- 'headwear-mohawk_green',
- 'headwear-mohawk_pink',
- 'headwear-moon_hat',
- 'headwear-pirates_hat',
- 'headwear-queens_crown',
- 'headwear-raincloud',
- 'headwear-rubber_duck',
- 'headwear-skully',
- 'headwear-tiara',
- 'headwear-tiny_crown',
- 'headwear-witchs_hat',
- 'headwear-wizards_hat',
- 'outerwear-hoodie',
- 'outerwear-hoodie_down',
- 'outerwear-punk_jacket',
-].each do |attribute|
-  spritesheet << Image.read( "./proof-moonbirds/attributes/#{attribute}.png" )
+cols = 20
+rows = (attributes.size/20)+1
+spritesheet = ImageComposite.new( cols, rows, width: 42, height: 42 )
+
+
+
+####
+#  add attributes
+attributes.each do |rec|
+  path = rec['path']
+  spritesheet << Image.read( "./attributes/#{path}" )
 end
 
+spritesheet.save( "./tmp/spritesheet.png" )
+spritesheet.zoom(2).save( "./tmp/spritesheet@2x.png" )
 
-spritesheet.save( "./tmp/moonbirds-spritesheet.png" )
-spritesheet.zoom(4).save( "./tmp/moonbirds-spritesheet@4x.png" )
 
 puts "bye"
 
