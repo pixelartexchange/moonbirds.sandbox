86a76ca46beb0274f0f75a054dac3f3429e24b4a
Gerald Bauer <gerald.bauer@gmail.com>
2022-05-18 18:47:50 +0200

    up

--- spritesheet/spritesheet.rb
+++ spritesheet/spritesheet.rb
@@ -19,6 +19,8 @@ rows = (attributes.size/20)+1
 spritesheet = ImageComposite.new( cols, rows, width: 42, height: 42 )
 
 
+meta = []  ## output meta(data) records
+
 
 ####
 #  add attributes
@@ -29,12 +31,63 @@ attributes.each do |rec|
   ##        moonbirds/attributes/beanie.png
 
   path = './' + parts[0] + '/attributes/' + parts[1..-1].join('/')
-
   spritesheet << Image.read( path )
+
+
+
+  name       = rec['name']
+  more_names = (rec['more_names'] || '').split( '|' )
+
+
+  if name.nil? || name.empty?
+    ## try to auto-fill name for path
+    filename   = parts[-1]
+    basename   = File.basename(filename, File.extname(filename))
+    dirname    = parts[-2]
+    collection = parts[0]
+
+    name =   if dirname == 'body' || dirname == 'body_legendary'
+                more_names << basename   ## bonus - add shortcut to more_names too
+                "body #{basename}"
+             elsif dirname == 'beak'
+                more_names << basename   ## bonus - add shortcut to more_names too
+                "beak #{basename}"
+             elsif dirname == 'eyes' && collection == 'moonbirds'
+                more_names << basename   ## bonus - add shortcut to more_names too
+                "eyes #{basename}"
+             else
+                basename
+             end
+  end
+
+
+
+  names =  [name] + more_names
+
+  ## normalize spaces in more names
+  names = names.map {|str| str.strip.gsub(/[ ]{2,}/, ' ' )}
+
+
+  meta << [meta.size,
+           names[0],
+           '?',   ## note: type to done (later)
+           names[1..-1].join( ' | '),
+          ]
 end
 
 spritesheet.save( "./tmp/spritesheet.png" )
 spritesheet.zoom(2).save( "./tmp/spritesheet@2x.png" )
+
+
+headers = ['id', 'name', 'type', 'more_names']
+File.open( "./tmp/spritesheet.csv", "w:utf-8" ) do |f|
+   f.write( headers.join( ', '))
+   f.write( "\n")
+   meta.each do |values|
+     f.write( values.join( ', ' ))
+     f.write( "\n" )
+   end
+end
 
 
 puts "bye"
