785aeb5f75cbeddda012041011f5012024072223
Gerald Bauer <gerald.bauer@gmail.com>
2022-11-01 18:08:08 +0100

    up

--- starbirds/collection.rb
+++
@@ -1,6 +0,0 @@
-
-COLLECTION = TokenCollection.new( 'starbirds', 3000,
-                token_base: 'ipfs://QmfX7KKqsHgdFNZNnVs7xzhMg4M5GmHxdPPJHZ5QSp2MaX/{id}.json',
-                format: '42x42',  ## note:  34px overflow!!!
-                source: '1000x1000',  ## '420x420'
-)
